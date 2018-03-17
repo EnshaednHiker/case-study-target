83b17a68d46e685f76443eda6c3262e435c8a211
EnshaednHiker <nbingham09@gmail.com>
2018-03-17 12:34:05 -0500

    set up main components structure, added styled-components

---
+++ components/MainImage/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Dom from 'react-dom';
+import styled from 'styled-components';
+
+export default class MainImage extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            
+            } 
+        }
+    
+
+    render() {
+       const Title = styled.h1 `
+            font-size: 26px;
+            line-height: 30px;
+       `;
+       const DivWrapper = styled.div `
+        
+       `;
+        return (
+                <DivWrapper className="main-image">
+                    <Title>{this.props.title}</Title>
+                </DivWrapper>
+            )
+    }
+}
--- public/main/index.js
+++ public/main/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Dom from 'react-dom';
 import fauxStore from '~/case-study/item-data.json';
+import MainImage from '~/components/MainImage'
+import css from '~/assets/styles/main.css'
 
 class Main extends React.Component {
     constructor(){
         super()
         this.state = {
-            title: fauxStore.CatalogEntryView[0].title 
+            title: fauxStore.CatalogEntryView[0].title,
+            primaryImage: fauxStore.CatalogEntryView[0].Images.PrimaryImage.image,
+            alternateImages: fauxStore.CatalogEntryView[0].Images.AlternateImages
         }
     }
     componentDidMount(){
         document.title = this.state.title;
     }
     render() {
+        
         console.log(fauxStore);
-        return (<h1>{this.state.title}</h1>)
+        return (
+            <MainImage title={this.state.title} primaryImage={this.state.primaryImage} alternateImages={this.state.alternateImages} />
+        )
     } 
 }
  
